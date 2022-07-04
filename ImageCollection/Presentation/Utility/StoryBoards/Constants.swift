350e690ed2b3247fcb57bd41006f9b15f80ce923
lionkiller900 <donalddaniels20@hotmail.com>
2022-07-04 13:25:24 +0100

    created more storyboards files such as Storyboard and constants

---
+++ ImageCollection/Presentation/Utility/StoryBoards/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageCollection
+//
+//  Created by Admin on 04/07/2022.
+//
+
+import Foundation
+
+struct Constants {
+   
+    // Column Size
+    static let  iPhoneColumnCount = 3
+    static let  iPadColumnCount = 4
+}
---
+++ ImageCollection/Presentation/Utility/StoryBoards/Storyboardable.swift
@@ -0,0 +1,10 @@
+//
+//  Storyboardable.swift
+//  ImageCollection
+//
+//  Created by Admin on 04/07/2022.
+//
+
+import Foundation
+
+
